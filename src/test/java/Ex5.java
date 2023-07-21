13d3434867f39f8e66b8c043b974e98dd41282df
aafonin <aafonin@neoflex.ru>
2023-07-21 10:22:34 +0300

    added task Ex5

---
+++ src/test/java/Ex5.java
@@ -0,0 +1,19 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Ex5 {
+
+    @Test
+    public void testRestAssured() {
+
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+
+        String answer = response.getString("messages[1].message");
+        System.out.println(answer);
+    }
+}
